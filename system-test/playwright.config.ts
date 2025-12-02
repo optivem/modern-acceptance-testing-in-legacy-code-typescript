6121584d324a46fb8c37d5f74b6b145502cde4f9
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-02 14:33:05 +0100

    refactor: remove baseURL configuration from Playwright setup

--- system-test/playwright.config.ts
+++ system-test/playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   workers: process.env.CI ? 2 : 4,
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:8082',
     trace: 'on-first-retry',
   },
   projects: [
