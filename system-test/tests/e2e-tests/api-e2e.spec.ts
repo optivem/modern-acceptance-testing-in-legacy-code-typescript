43975ab826ca05717e2449cb7b4d931fce2d3000
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-02 14:35:13 +0100

    refactor: add result matchers setup to API and UI E2E tests

--- system-test/tests/e2e-tests/api-e2e.spec.ts
+++ system-test/tests/e2e-tests/api-e2e.spec.ts
@@ -3,6 +3,9 @@ import { DriverFactory } from '../../core/drivers/DriverFactory.js';
 import { defineE2eTests } from './e2e-tests.js';
 import { defineApiE2eTests } from './api-e2e-tests.js';
 import { Closer } from '../../core/drivers/commons/clients/Closer.js';
+import { setupResultMatchers } from '../../core/matchers/resultMatchers.js';
+
+setupResultMatchers();
 
 const test = base.extend({
     shopDriver: async ({}, use: any) => {
--- system-test/tests/e2e-tests/ui-e2e.spec.ts
+++ system-test/tests/e2e-tests/ui-e2e.spec.ts
@@ -2,6 +2,9 @@ import { test as base } from './e2e-tests.js';
 import { DriverFactory } from '../../core/drivers/DriverFactory.js';
 import { defineE2eTests } from './e2e-tests.js';
 import { Closer } from '../../core/drivers/commons/clients/Closer.js';
+import { setupResultMatchers } from '../../core/matchers/resultMatchers.js';
+
+setupResultMatchers();
 
 const test = base.extend({
     shopDriver: async ({}, use: any) => {
