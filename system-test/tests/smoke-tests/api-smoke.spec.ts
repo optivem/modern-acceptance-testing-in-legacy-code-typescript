39fce7a23aa96e03b8405fbc9edbb7fed97c00da
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-02 14:41:06 +0100

    refactor: add result matchers setup to API and UI smoke tests

--- system-test/tests/smoke-tests/api-smoke.spec.ts
+++ system-test/tests/smoke-tests/api-smoke.spec.ts
@@ -2,6 +2,9 @@ import { test as base } from '../fixtures.js';
 import { DriverFactory } from '../../core/drivers/DriverFactory.js';
 import { defineShopSmokeTests } from './shop-smoke-tests.js';
 import { Closer } from '../../core/drivers/commons/clients/Closer.js';
+import { setupResultMatchers } from '../../core/matchers/resultMatchers.js';
+
+setupResultMatchers();
 
 const test = base.extend({
     shopDriver: async ({}, use: any) => {
