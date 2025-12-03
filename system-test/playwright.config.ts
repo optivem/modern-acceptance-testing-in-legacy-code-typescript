f18cff0f76cfa51879716308f551c1ef31f13d89
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-03 18:48:54 +0100

    feat: implement custom test reporter for enhanced output and clarity

--- system-test/playwright.config.ts
+++ system-test/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : 4,
-  reporter: [['list'], ['html', { open: 'never' }]],
+  reporter: [['./custom-reporter.js'], ['html', { open: 'never' }]],
   use: {
     trace: 'on-first-retry',
   },
